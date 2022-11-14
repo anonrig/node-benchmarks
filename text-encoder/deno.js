4fcaf5c673b492c2466cfadb308777aab891a84b
Yagiz Nizipli <yagiz@nizipli.com>
2022-11-14 12:01:50 -0500

    feat: add buffer.from to text-encoder bench

--- text-encoder/deno.js
+++ text-encoder/deno.js
@@ -1,9 +1,11 @@
 import { bench, run } from "https://esm.sh/mitata"
+import { Buffer } from "https://deno.land/std@0.164.0/node/buffer.ts"
 
 const encoder = new TextEncoder()
 const input = 'hello-world'.repeat(1000)
 const subarray = new Uint8Array(1000)
 
+bench('Buffer.from', () => Buffer.from(input, 'utf8'))
 bench('TextEncoder.encode', () => encoder.encode(input))
 bench('TextEncoder.encodeInto', () => encoder.encodeInto(input, subarray))
 
--- text-encoder/index.mjs
+++ text-encoder/index.mjs
@@ -1,9 +1,11 @@
 import { bench, run } from "mitata"
+import { Buffer } from 'node:buffer'
 
 const encoder = new TextEncoder()
 const input = 'hello-world'.repeat(1000)
 const subarray = new Uint8Array(1000)
 
+bench('Buffer.from', () => Buffer.from(input, 'utf8'))
 bench('TextEncoder.encode', () => encoder.encode(input))
 bench('TextEncoder.encodeInto', () => encoder.encodeInto(input, subarray))
 
