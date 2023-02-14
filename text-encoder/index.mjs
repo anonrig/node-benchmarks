b2b630ae0977321405692985adb9cc9dc043de02
Yagiz Nizipli <yagiz@nizipli.com>
2023-02-14 13:48:03 -0500

    feat: add more alternatives to text-encoder

--- text-encoder/index.mjs
+++ text-encoder/index.mjs
@@ -1,12 +1,15 @@
 import { bench, run } from "mitata"
 import { Buffer } from 'node:buffer'
 
-const encoder = new TextEncoder()
-const input = 'hello-world'.repeat(1000)
-const subarray = new Uint8Array(1000)
+let u = new Uint8Array(2048);
+let b = Buffer.alloc(2048);
+let uf8Write = Buffer.prototype.utf8Write;
+let te = new TextEncoder();
 
-bench('Buffer.from', () => Buffer.from(input, 'utf8'))
-bench('TextEncoder.encode', () => encoder.encode(input))
-bench('TextEncoder.encodeInto', () => encoder.encodeInto(input, subarray))
+bench('buffer.write', () => b.write('hello world', 10))
+bench('buffer.utf8Write', () => b.utf8Write('hello world', 10, 0xffffff))
+bench('utf8Write.call', () => uf8Write.call(u, 'hello world', 10, 0xffffff))
+bench('TextEncoder.encodeInto with subarray(10)', () => te.encodeInto('hello world', u.subarray(10)))
+bench('TextEncoder.encodeInto', () => te.encodeInto('hello world', u))
 
-await run()
+await run();
