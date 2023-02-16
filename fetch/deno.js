f22eb91fb06f15314274361908a9edc1b6296f01
Yagiz Nizipli <yagiz@nizipli.com>
2023-02-16 09:40:52 -0500

    feat: add fetch benchmarks

---
+++ fetch/deno.js
@@ -0,0 +1,20 @@
+import { bench, run } from "https://esm.sh/mitata"
+
+const count = 100;
+
+bench('noop', () => {});
+bench('noop', () => {});
+
+bench(`fetch(https://example.com) x ${count}`, async () => {
+  const requests = new Array(count);
+
+  for (let i = 0; i < requests.length; i++) {
+    requests[i] = fetch(`https://www.example.com/?cachebust=${i}`).then((r) =>
+      r.text(),
+      );
+  }
+
+  await Promise.all(requests);
+});
+
+await run();
