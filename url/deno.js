29f5a6db50c92abfb743855b1ce5e9438724c17c
Yagiz Nizipli <yagiz@nizipli.com>
2023-02-27 10:36:35 -0500

    perf: remove `url.parse` since it's no longer a competition

--- url/deno.js
+++ url/deno.js
@@ -1,24 +1,11 @@
-import { bench, run, group } from "https://esm.sh/mitata"
-import { parse } from 'node:url'
+import { bench, run } from "https://esm.sh/mitata"
 
-bench('noop', () => {});
-bench('noop', () => {});
+bench('noop', () => { });
+bench('noop', () => { });
 
-group('basic domain', () => {
-  bench('new URL', () => new URL('https://example.com?application_id=123456789'))
-  bench('parse', () => parse('https://example.com?application_id=123456789'))
-})
-group('ipv4', () => {
-  bench('new URL', () => new URL('http://192.168.1.1'))
-  bench('parse', () => parse('http://192.168.1.1'))
-})
-group('ipv6', () => {
-  bench('new URL', () => new URL('http://[2606:4700:4700::1111]'))
-  bench('parse', () => parse('http://[2606:4700:4700::1111]'))
-})
-group('complex', () => {
-  bench('new URL', () => new URL('postgresql://other:9818274x1!!@localhost:5432/otherdb?connect_timeout=10&application_name=myapp'))
-  bench('parse', () => parse('postgresql://other:9818274x1!!@localhost:5432/otherdb?connect_timeout=10&application_name=myapp'))
-})
+bench('basic domain', () => new URL('https://example.com?application_id=123456789'))
+bench('ipv4', () => new URL('http://192.168.1.1'))
+bench('ipv6', () => new URL('http://[2606:4700:4700::1111]'))
+bench('complex', () => new URL('postgresql://other:9818274x1!!@localhost:5432/otherdb?connect_timeout=10&application_name=myapp'))
 
 await run()
