82991267c063a1840c65e2c86a26ef296328e3bc
Yagiz Nizipli <yagiz@nizipli.com>
2023-03-30 14:49:44 -0400

    feat: use more realistic inputs for benchmarking urls

--- url/deno.js
+++ url/deno.js
@@ -1,11 +1,21 @@
 import { bench, run } from "https://esm.sh/mitata"
+import fs from 'node:fs'
+
+const dataset = new URL('dataset.txt', import.meta.url)
+const file = fs.readFileSync(dataset, 'utf-8')
+const lines = file.split('\n')
 
 bench('noop', () => { });
 bench('noop', () => { });
 
-bench('basic domain', () => new URL('https://example.com?application_id=123456789'))
-bench('ipv4', () => new URL('http://192.168.1.1'))
-bench('ipv6', () => new URL('http://[2606:4700:4700::1111]'))
-bench('complex', () => new URL('postgresql://other:9818274x1!!@localhost:5432/otherdb?connect_timeout=10&application_name=myapp'))
+bench('new URL', () => {
+  for (let line of lines) {
+    try {
+      new URL(line);
+    } catch {
+      // do nothing
+    }
+  }
+})
 
 await run()
